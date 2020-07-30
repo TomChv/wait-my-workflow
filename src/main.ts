881d616478599d81c653cb6c716045f0b728827a
Tom Chauveau <tom.chauveau@epitech.eu>
2020-07-30 10:34:04 +0200

    fix: norm

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
-import * as core from '@actions/core'
-import {context, GitHub} from '@actions/github'
-import {poll} from './poll'
+import * as core from '@actions/core';
+import {context, GitHub} from '@actions/github';
+import {poll} from './poll';
 
 async function run(): Promise<void> {
-  try {
-    const token = core.getInput('token', {required: true})
+	try {
+		const token = core.getInput('token', {required: true});
 
-    const result = await poll({
-      client: new GitHub(token),
-      log: msg => core.info(msg),
+		const result = await poll({
+			client: new GitHub(token),
+			log: msg => core.info(msg),
 
-      checkName: core.getInput('checkName', {required: true}),
-      owner: core.getInput('owner') || context.repo.owner,
-      repo: core.getInput('repo') || context.repo.repo,
-      ref: core.getInput('ref') || context.sha,
+			checkName: core.getInput('checkName', {required: true}),
+			owner: core.getInput('owner') || context.repo.owner,
+			repo: core.getInput('repo') || context.repo.repo,
+			ref: core.getInput('ref') || context.sha,
 
-      timeoutSeconds: parseInt(core.getInput('timeoutSeconds') || '600'),
-      intervalSeconds: parseInt(core.getInput('intervalSeconds') || '10')
-    })
+			timeoutSeconds: parseInt(core.getInput('timeoutSeconds') || '600'),
+			intervalSeconds: parseInt(core.getInput('intervalSeconds') || '10')
+		});
 
-    core.setOutput('conclusion', result)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+		core.setOutput('conclusion', result);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
-run()
+run();
